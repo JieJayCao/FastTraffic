06e300203f404bbbd9d1873584c307628e7bb728
巧克力可颂 <45455827+JieJayCao@users.noreply.github.com>
2024-02-21 10:39:58 +0800

    Update and rename fasttraffic.py to FastTraffic.py

--- models/FastTraffic.py
+++ models/FastTraffic.py
@@ -9,7 +9,7 @@ class Config(object):
 
     """配置参数"""
     def __init__(self, dataset, embedding):
-        self.model_name = 'FastText'
+        self.model_name = 'FastTraffic'
         self.train_path = dataset + '/data/train.txt'                                # 训练集
         self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
         self.test_path = dataset + '/data/test.txt'                                  # 测试集
